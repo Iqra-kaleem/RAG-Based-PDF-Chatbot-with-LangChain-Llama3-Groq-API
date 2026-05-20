e0abfa998618e46039efc451bd30c4b3c24415e4
Iqra Kaleem <iqrakaleem7054@gmail.com>
2026-05-21 03:07:29 +0530

    added document retrieval , response timing and similarity search display

--- app.py
+++ app.py
@@ -32,3 +32,44 @@ Question: {input}
 
 """    
 )
+
+def vector_embedding():
+
+    if "vector" not in st.session_state:
+    
+     st.session_state.embeddings= OpenAIEmbeddings()
+     st.session_state.loader= PyPDFDirectoryLoader("./Data") ##Data Ingestion
+     st.session_state.docs=st.session_state.loader.load() ##Document Loading
+     st.session_state.text_splitter= RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200) ##Chunk Creation
+     st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs) ##Splitting
+     st.session_state.vector= FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings) ##vector openai embeddings
+
+
+
+
+prompt1= st.text_input("Enter your question from documents")
+
+if st.button("Documents Embedding"):
+    vector_embedding()
+    st.write("Vector Store DB is Ready")
+
+import time
+
+document_chain= create_stuff_documents_chain(llm,prompt)
+retriever= st.session_state.vectors.as_retriever()
+retrieval_chain=create_retrieval_chain(retriever, document_chain)
+
+if prompt1:
+   start=time.process_time()
+   response= retrieval_chain.invoke({'input':prompt1})
+   print("Response time:",time.process_time()-start)
+   st.write(response['answer'])
+
+   # with a streamlit expander
+   with st.expander("Document Similarity Search"):
+      # find the relevant chunks
+      for i , doc in enumerate(response["context"]):
+         st.write(doc.page_content)
+         st.write("-----------------------")
+
+
